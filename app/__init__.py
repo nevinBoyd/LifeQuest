f6e12dd791f35e5dfe2ced6dd578a21b44697b21
Nevin <n.boyd1@yahoo.com>
2025-12-15 12:36:00 -0800

    Add Flask-Migrate integration

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
-
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
@@ -13,5 +14,6 @@ def create_app():
 
     # Initialize extensions
     db.init_app(app)
-
+    migrate.init_app(app, db)
+    
     return app
